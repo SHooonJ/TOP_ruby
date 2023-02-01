d29567bcc62899225a674cd087ad4f4238973e70
shjung <andrewstudy629@gmail.com>
2023-02-01 00:36:22 -0800

    Create mastermind class with ability to ask for key

--- OOP_Basics/master_mind/master_mind.rb
+++ OOP_Basics/master_mind/master_mind.rb
@@ -1,4 +1,13 @@
+COLORS = ["red","blue","green","yellow","orange","pink"]
+PEGS   = ["black","white"]
+
 class BOARD
+    attr_accessor :guess_rows, :key_row
+    attr_reader :colors, :pegs
+
+    @@colors = ["red","blue","green","yellow","orange","pink"]
+    @@pegs = ["black","white"]
+
     def initialize()
         @guess_rows = Array.new(10){Array.new(8,0)}
         @key_row = Array.new(4,0)
@@ -12,8 +21,26 @@ class BOARD
         print @key_row
         puts
     end 
+end
+
+class MASTERMIND
+    def createKey()
+        puts "Enter the key"
+        (0..3).each do |i|
+            puts "Enter key for index #{i}"
+            key_input = gets.chomp
+            check_input(key_input)
+        end 
+    end
+
+    def check_input(input)
+        COLORS.include?(input)
+    end
 end 
 
 
-mastermind = BOARD.new()
-mastermind.draw
+mastermind_board = BOARD.new()
+mastermind_board.draw
+
+mastermind = MASTERMIND.new()
+mastermind.createKey
